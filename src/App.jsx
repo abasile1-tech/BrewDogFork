e9f2c06f7ae579bfdbf737aa2351a38c88095f68
John <ashton.basile@gmail.com>
2023-08-29 11:53:42 +0100

    I cleaned up the routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,10 @@ import { Routes, Route, Outlet, Link } from "react-router-dom";
 function App() {
   return (
     <div>
-      <h1>React Router Example</h1>
-
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<BeerContainer />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
-
-          {/* Using path="*"" means "match anything", so this route
-                  acts like a catch-all for URLs that we don't have explicit
-                  routes for. */}
+          <Route path="favorites" element={<Favorites />} />
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
@@ -29,44 +19,27 @@ function App() {
 function Layout() {
   return (
     <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
       <nav>
         <ul type="none">
           <li>
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/favorites">Favorites</Link>
           </li>
         </ul>
       </nav>
 
       <hr />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
       <Outlet />
     </div>
   );
 }
 
-function About() {
+function Favorites() {
   return (
     <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
+      <h2>Favorites</h2>
     </div>
   );
 }
